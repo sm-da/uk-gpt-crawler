030ded921c41044138e7fc18f7d51ace1c48439a
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:34:54 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://mon.gov.ua/en/tag/preschool-education?&type=all&tag=Preschool%20Education",
+  url: "https://mon.gov.ua/sitemap-kitsoft-mediagallery-models-mediagallery-1.xml",
   match: "https://mon.gov.ua/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 9999999999,
   outputFileName: "mon-gov-ua.json",
   maxTokens: 2000000,
-  selector: "body",
+  selector: "html",
 };
