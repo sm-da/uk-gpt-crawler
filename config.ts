118d73b6d667f53af6bd1bffe6af1b6ae7a430c4
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 16:56:55 +0000

    uf

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
   url: "https://www.kmu.gov.ua/en",
-  match: "https://www.kmu.gov.ua/en/**",
+  match: "https://www.kmu.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "kmu.json",
   maxTokens: 2000000,
