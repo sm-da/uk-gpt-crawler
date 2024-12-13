78e0d9a151923af6e2dde85c0a3984777708b2da
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 22:16:03 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.mspu.gov.ua/",
-  match: "https://www.mspu.gov.ua/**",
+  url: "https://mspu.gov.ua/",
+  match: "https://mspu.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "mspu.json",
   maxTokens: 2000000,
