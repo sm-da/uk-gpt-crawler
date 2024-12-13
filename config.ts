eb07b977f83328898919fb3f556bdfe19d83212c
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:08:45 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://thedigital.gov.ua/",
   match: "https://thedigital.gov.ua/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "thedigital.json",
   maxTokens: 2000000,
   selector: "body",
