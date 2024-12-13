58c3beb8ca457507f9916276f06a3fefcc3e435c
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 21:45:41 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://www.kyivpost.com/",
+  match: "https://www.kyivpost.com/**",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "kyivpost-com.json",
   maxTokens: 2000000,
   selector: "body"
 };
