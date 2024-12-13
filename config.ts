b6fcb131e9f734021003da3f45814caa9dcb77e5
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 22:10:58 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.mtu.gov.ua/",
   match: "https://www.mtu.gov.ua/**",
-  maxPagesToCrawl: 99999999,
+  maxPagesToCrawl: 9999999,
   outputFileName: "mtu-gov-ua.json",
   maxTokens: 2000000,
   selector: "body",
