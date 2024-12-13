8f3a9b34f6d296c960e5edf6733f347a7c3b9a93
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 20:22:24 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://mfa.gov.ua/en/about-ukraine",
   match: "https://mfa.gov.ua/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "mfa-gov-ua.json",
   maxTokens: 2000000,
   selector: "html",
