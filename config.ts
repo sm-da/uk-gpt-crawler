5a8747fabf95bf9e5029649b3ac1d43dc073c2f3
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:37:39 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://mon.gov.ua/sitemap-kitsoft-npa-models-act-1.xml",
   match: "https://mon.gov.ua/**",
-  maxPagesToCrawl: 9999999999,
+  maxPagesToCrawl: 999999999,
   outputFileName: "mon-gov-ua.json",
   maxTokens: 2000000,
   selector: "html",
