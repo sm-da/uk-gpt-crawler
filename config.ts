a482edcc21788a6c7dd346b95ef0a25d7646a3df
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 20:45:44 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://health.gov.ua/",
+  match: "https://health.gov.ua/**",
+  maxPagesToCrawl: 999999999,
+  outputFileName: "health-gov-ua.json",
   maxTokens: 2000000,
   selector: "body"
 };
