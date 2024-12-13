77710291afe9e9d8111d3b9b30d11760b6153696
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:27:01 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://mon.gov.ua/",
+  url: "https://mon.gov.ua/en/tag/preschool-education?&type=all&tag=Preschool%20Education",
   match: "https://mon.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "mon-gov-ua.json",
