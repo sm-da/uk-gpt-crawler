e69446ff4e20811476540b5aff224c4eed3ac847
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:25:28 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://me.gov.ua/Documents/SiteMap?lang=en-GB",
   match: "https://me.gov.ua/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "me-gov-ua.json",
   maxTokens: 2000000,
   selector: "body",
