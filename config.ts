79610888ce5cc7533b3de3acfbd471222a36d0e9
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 21:03:46 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.mvs.gov.ua/en",
-  match: "https://www.mvs.gov.ua/**",
+  url: "hhttps://infotech.gov.ua/",
+  match: "https://infotech.gov.ua/**",
   maxPagesToCrawl: 99999999,
-  outputFileName: "mvs-gov-ua.json",
+  outputFileName: "infotech.json",
   maxTokens: 2000000,
   selector: "body",
 };
