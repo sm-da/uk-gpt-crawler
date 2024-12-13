dfad17ea42f51d2fa9bc7840dde4c20689c0e2de
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 19:26:06 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://mcsc.gov.ua/",
+  url: "https://mcsc.gov.ua/  ",
   match: "https://mcsc.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "mcsc.json",
