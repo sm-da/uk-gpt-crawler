1e4e1a8a1ac45785c97ab812cd70ebbe0ca411e4
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 15:51:59 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://minagro.gov.ua/gromadyanam/publichna-informaciya",
+  url: "https://minagro.gov.ua/timeline?&type=posts&category_id=10",
   match: "https://minagro.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "minagro.json",
