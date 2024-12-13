1bd58471b0ca3926236de4473d0d358a731d699e
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 22:55:25 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://mva.gov.ua/en",
   match: "https://mva.gov.ua/en**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "mva-gov-ua.json",
   maxTokens: 2000000,
   selector: "body",
