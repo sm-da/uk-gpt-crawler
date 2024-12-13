78b37002736e3b3dc1b7bb3f8f794eeed32a9b54
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 22:00:01 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.msp.gov.us/",
-  match: "https://www.msp.gov.us/**",
+  url: "https://www.msp.gov.ua/",
+  match: "https://www.msp.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "msp-gov-us.json",
   maxTokens: 2000000,
-  selector: "body"
+  selector: "body",
 };
