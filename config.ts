c0c183f45f0bb3f8dddd8174d7ae5de6a651d747
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 15:50:15 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://minagro.gov.ua/tag/roslinnictvo",
-  match: "https://minagro.gov.ua/tag/**",
+  url: "https://minagro.gov.ua/investoram/prezentatsiia-stratehiia-rozvytku-ahropromyslovoho-kompleksu",
+  match: "https://minagro.gov.ua/**",
   maxPagesToCrawl: 999999999,
   outputFileName: "minagro.json",
   maxTokens: 2000000,
