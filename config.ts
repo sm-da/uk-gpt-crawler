9845a76bdb9bef316cd5ac583f2d10d09c20e86d
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 21:15:05 +0000

    uf

--- config.ts
+++ config.ts
@@ -3,8 +3,8 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://platformonline.com.ua/",
   match: "https://platformonline.com.ua/**",
-  maxPagesToCrawl: 999999999,
+  maxPagesToCrawl: 99999999,
   outputFileName: "platformonline.json",
   maxTokens: 2000000,
-  selector: "body"
+  selector: "body",
 };
