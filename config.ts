6a0be74b3e6d9785c38e5bd2b0a6d9b7523d9607
SuperMarioAI <17409586+supermario-ai@users.noreply.github.com>
2024-12-13 21:10:53 +0000

    uf

--- config.ts
+++ config.ts
@@ -1,10 +1,10 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
+  url: "https://www.minjust.gov.ua/",
+  match: "httpshttps://www.minjust.gov.ua/**",
+  maxPagesToCrawl: 9999999999,
+  outputFileName: "minjust-gov-ua.json",
   maxTokens: 2000000,
   selector: "body"
 };
